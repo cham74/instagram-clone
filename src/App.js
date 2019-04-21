8557f2018bade5dd6d7ad5bcc6110a95e71e9f2d
Jacob Lutin <jacobl1933@gmail.com>
2019-04-21 19:50:13 +0300

    config pusher on the client

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from "react-apollo";
+import Pusher from 'pusher-js';
 
 import './App.css';
 import Header from './components/Header';
@@ -10,17 +11,30 @@ const client = new ApolloClient({
   uri : "http://localhost:4000/graphql"
 })
 
-const App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <div className="App">
-        <Header />
-        <section className="App-main">
-          <Posts />
-        </section>
-      </div>
-    </ApolloProvider>
-  );
-};
+// create component
+class App extends Component {
+  constructor(){
+    super();
+    // connect to pusher
+    this.pusher = new Pusher("PUSHER_APP_KEY", {
+      cluster: 'eu',
+      encrypted: true
+    });
+  }
+
+  render(){
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Header />
+          <section className="App-main">
+            {/* pass the pusher object and apollo to the posts component */}
+            <Posts pusher={this.pusher} apollo_client={client}/>
+          </section>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
 
 export default App;
--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,41 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+
 import "./Posts.css";
 import Post from "../Post";
 
-const Posts = () => {
-  return (
-    <Query
-      query={gql`
-        {
-          posts(user_id: "a"){
-            id
-            user{
-              nickname
-              avatar
-            }
-            image
-            caption
-          }
-        }
-      `}
-    >
-    {({loading, error, data}) => {
-      if (loading) return <p>Loading Posts...</p>;
-      if (error) return <p>Error Fetching Posts...</p>;
-      let posts = data.posts;
+class Posts extends Component {
+  constructor(){
+    super();
+    this.state = {
+      posts : []
+    }
+  }
 
-      return <div className="Posts">
-        {
-        posts.map(
-          post => <Post nickname={post.user.nickname} avatar={post.user.avatar} image={post.image} caption={post.caption} key={post.id}/>
-          )
-        }
-      </div>;
-    }}
-    </Query>
-  );
+  componentDidMount(){
+    // fetch the initial posts 
+    this.props.apollo_client
+      .query({ 
+        query:gql`
+          {
+            posts(user_id: "a"){
+              id
+              user{
+                nickname
+                avatar
+              }
+              image
+              caption
+            }
+          } 
+        `})
+      .then(response => {
+        this.setState({ posts: response.data.posts});
+      });
+
+    this.posts_channel = this.props.pusher.subscribe('posts-channel');
+
+    // listen for a new post
+    this.posts_channel.bind("new-post", data => {
+        this.setState({ posts: this.state.posts.concat(data.post) });
+      }, this);
+  }
+
+  render(){
+    return (
+      <div className="Posts">
+        {this.state.posts.map(post => <Post nickname={post.user.nickname} avatar={post.user.avatar} image={post.image} caption={post.caption} key={post.id}/>)}
+      </div>
+    );
+  }
 }
 
 export default Posts;
